f53245121a21091f0127248a0bf6369de378f159
jntakpe <jntakpe@gmail.com>
2016-12-26 17:22:25 +0100

    Ajout exemple const

---
+++ js/const.js
@@ -0,0 +1,14 @@
+const username = 'jntakpe';
+// username = 'gpeel';
+
+const user = {
+    name: 'jntakpe'
+};
+
+user.name = 'jguerrin';
+
+const arr = [];
+
+arr.push(user);
+
+console.log(arr[0]);
