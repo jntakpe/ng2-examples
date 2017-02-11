2d786a1e26769abf7f16c3bb05d360d58fac70b5
jntakpe <jntakpe@gmail.com>
2017-02-11 22:00:26 +0100

    reactive form

--- ng2/src/app/react-form/react-form.component.ts
+++ ng2/src/app/react-form/react-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
@@ -6,15 +6,11 @@ import {FormBuilder, FormGroup} from '@angular/forms';
   templateUrl: './react-form.component.html',
   styleUrls: ['./react-form.component.css']
 })
-export class ReactFormComponent implements OnInit {
-
+export class ReactFormComponent {
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
-  }
-
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
+  constructor(formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
       username: '',
       password: ''
     });
@@ -23,5 +19,4 @@ export class ReactFormComponent implements OnInit {
   register() {
     console.log(this.loginForm.value);
   }
-
 }
