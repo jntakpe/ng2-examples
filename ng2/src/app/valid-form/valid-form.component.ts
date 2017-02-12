9602217e85c7bf9356ff3f1cbc9818d21c2b443c
jntakpe <jntakpe@gmail.com>
2017-02-12 13:54:46 +0100

    Fin form

--- ng2/src/app/valid-form/valid-form.component.ts
+++ ng2/src/app/valid-form/valid-form.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-valid-form',
@@ -11,10 +11,13 @@ export class ValidFormComponent {
   loginForm: FormGroup;
 
   constructor(formBuilder: FormBuilder) {
+    const isKnowUser = (control: FormControl) => ['jntakpe', 'gpeel'].filter(v => v === control.value).length ? null : {unknownUser: true};
     this.loginForm = formBuilder.group({
-      username: formBuilder.control('', [Validators.required, Validators.minLength(3)]),
+      username: formBuilder.control('', [Validators.required, Validators.minLength(3), isKnowUser]),
       password: formBuilder.control('', Validators.required)
     });
+    this.loginForm.valueChanges.subscribe(form => console.log(form));
+    this.loginForm.get('username').valueChanges.subscribe(username => console.log(username));
   }
 
   register() {
